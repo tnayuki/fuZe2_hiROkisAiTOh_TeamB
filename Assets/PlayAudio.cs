166530285b4dda89a419bc3ae9c9c832712adc49
Toru Nayuki <tnayuki@MacBook-Air.local>
2013-11-18 00:22:32 +0900

    大前先生作成のPlayAudioスクリプトを追加。

---
+++ Assets/PlayAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudio : MonoBehaviour {
+
+	void DoPlayAudio(float value) {
+
+		audio.volume = value;
+		if( !audio.isPlaying ) {
+			audio.Play();
+		}		
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
