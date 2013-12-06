9ccb2e5812d98ad4e26cf62e4dcf800a447a3569
Toru Nayuki <tnayuki@MacBook-Air.local>
2013-12-06 20:11:00 +0900

    輪っかシェーダー

---
+++ Assets/TeamB/Image Effects/Rings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Rings : ImageEffectBase {
+	public Vector2 center = new Vector2(0, 0);
+	public float turningRadius = 1.0f;
+	public float turningSpeed = 1.0f;
+	public Color color = new Color(1, 1, 1, 1);
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		material.SetVector("_Center", center);
+		material.SetFloat("_TurningSpeed", turningSpeed);
+		material.SetFloat("_TurningRadius", turningRadius);
+		material.SetColor("_Color", color);
+
+		Graphics.Blit(source, destination, material);
+	}
+}
